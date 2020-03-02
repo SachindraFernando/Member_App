8c3503f02d2ceda8d2edb62d1774f146296fe9d6
Sachindra <sachindrafernando3@gmail.com>
2020-03-02 20:00:16 +0530

    member app

---
+++ members.js
@@ -0,0 +1,21 @@
+const members = [
+    {
+        id:1,
+        name:'sachi',
+        email:'sachi@gmail.com',
+        status:'active'
+    },
+    {
+        id:2,
+        name:'sachi',
+        email:'sachi@gmail.com',
+        status:'active'
+    },
+    {
+        id:3,
+        name:'sachi',
+        email:'sachi@gmail.com',
+        status:'inactive'
+    }
+];
+module.exports = members;
